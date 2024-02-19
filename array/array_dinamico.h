f03597a32e8e2ec00a66f96b4c82601c56855be6
bgmorettu <160488352+bgmorettu@users.noreply.github.com>
2024-02-19 08:38:41 -0300

    Update array_dinamico.h

--- array/array_dinamico.h
+++ array/array_dinamico.h
@@ -4,4 +4,8 @@
 typedef struct ArrayDinamico ArrayDinamico;
 ArrayDinamico* array_dinamico_create();
 void array_dinamico_destroy(ArrayDinamico* array);
-voyd array_dinamico_add (ArrayDinamico
+voyd array_dinamico_add (ArrayDinamico* array, Element element);
+void array_dinamico_print (ArrayDinamico* array);
+int array_dinamico_size (arrayDinamico* array);
+void array_dinamico_set (arrayDinamico* array, int pos, Elementet element);
+
